82938d3bb637e140dce2ffb8d5f9121a51f35965
Likith Gowda K <likithkrish31@gmail.com>
2020-09-08 16:24:04 +0530

    Create method_hidding.cs

---
+++ method_hidding.cs
@@ -0,0 +1,51 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace helloworld
+{
+    public class employee
+    {
+         public string fn;
+         public string ln;
+
+        public employee(string firstname,string lastname)
+        {
+            this.fn = firstname;
+            this.ln = lastname;
+        }
+        public void printname()
+            //this method is hidden 
+        {
+            Console.WriteLine("the name is{0}", this.fn + this.ln);
+        }
+    }
+    public class Fulltime : employee
+    {
+        public float monthlySalary;
+
+        public Fulltime(string firstname, string lastname,float monthlySalary): base (firstname,lastname)
+        {
+            this.monthlySalary = monthlySalary;
+        }
+        public new void printname()
+            //writing code to hide parent method
+        {
+            Console.WriteLine("the name is{0}thats all", this.fn + this.ln);
+        }
+        public void printsalary()
+        {
+          
+            Console.WriteLine("the name is{0}", this.fn + this.ln + this.monthlySalary);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Fulltime f = new Fulltime("liki","gow",50000);
+            f.printsalary();
+        }
+    }
+}
