31721c28d8668ddff4ee718f7e3b40628010de8a
Likith Gowda K <likithkrish31@gmail.com>
2020-09-14 21:28:08 +0530

    Create AutoImplementedProperties.cs

---
+++ AutoImplementedProperties.cs
@@ -0,0 +1,84 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+//abstraction Using properties
+namespace helloworld
+{
+
+    class Student
+    {
+        private int _id;
+        private string _Name;
+        public int _passMark = 35;
+
+        //auto Implemented Prooerties
+        public string City
+            //here complier will create properties and private city feild If nothing is set no response in output
+        {
+            get;
+            set;
+        }
+
+        //since pass mark is only get we use read only property
+        public int Passmark
+        {
+            get
+            {
+                return this._passMark;
+            }
+        }
+
+        //PS:no () for properties
+        public string Name
+        {
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                    throw new Exception("Name Cannot be Null");
+                this._Name = value;
+            }
+            get
+            {
+                if (string.IsNullOrEmpty(this._Name))
+                    return "No Name";
+                else
+                    return this._Name;
+            }
+        }
+
+        public int Id
+        {
+            set
+            {
+
+                if (value <= 0)
+                    throw new Exception("Id cannot be negative");
+                this._id = value;
+            }
+            get
+            {
+                return this._id;
+            }
+        }
+      
+    }
+    class Program
+    { 
+        public static void Main()
+            {
+            Student s = new Student();
+            //assinging values should be done explicitly with== and not like method
+            s.Name="likith";
+            s.Id = 100;
+            s.City = "banglore";
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.Id);
+            Console.WriteLine(s.City);
+        }
+       
+        }
+    }
+
