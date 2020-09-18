8c92eeedac0fd7c1a4482f8882756dbec1f0ca48
Likith Gowda K <likithkrish31@gmail.com>
2020-09-18 10:08:40 +0530

    Create explicit_Interface_implementation.cs

---
+++ explicit_Interface_implementation.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+//Explicit Interface Implementation , when two interfaces have same method we use explicit implementation
+namespace helloworld
+{
+    interface ICustomer
+    {
+        void print();
+    }
+    interface IStudent
+    {
+        void print();
+    }
+    class customer : ICustomer,IStudent 
+    {
+         void IStudent.print()
+        {
+            throw new NotImplementedException();
+        }
+
+        //explicit implementation doesn't have access modifier 
+        void ICustomer.print()
+        {
+            Console.WriteLine("Interface");
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            customer c = new customer();
+            //to call explicit interface we typecast first
+            ((ICustomer)c).print();
+
+        }
+    }
+}
+
